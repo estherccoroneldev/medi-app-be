f90420a313e284752c8fc3919a94bdc2c51964a3
Leomariyer Coronel <lestherccoronel@gmail.com>
2025-01-31 15:14:38 -0300

    connect to the database

---
+++ database/database.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/medDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}); // connect to database
+
+const db = mongoose.connection; // get default connection
+
+db.on("error", console.error.bind(console, "connection error:")); // add error handler
+
+db.once("open", () => {
+    console.log("Connected to database successfully"); // log success message
+}); // add open handler
+
+export default db; // export default connection
